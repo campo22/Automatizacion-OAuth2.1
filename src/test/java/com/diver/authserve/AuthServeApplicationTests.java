603ed3b5328d33b8955cf5995a62d072cf77ee08
campo22 <miguelcampodiaz@gmail.com>
2024-09-25 08:53:57 -0500

    primer commit

---
+++ src/test/java/com/diver/authserve/AuthServeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diver.authserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
