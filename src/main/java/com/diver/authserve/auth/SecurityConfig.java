73cda5752dbf48638f792b6cecd3bf45c4714493
campo22 <miguelcampodiaz@gmail.com>
2024-09-26 11:26:59 -0500

    subiendo congiguracion

---
+++ src/main/java/com/diver/authserve/auth/SecurityConfig.java
@@ -0,0 +1,2 @@
+package com.diver.authserve.auth;public class SecurityConfig {
+}
--- src/test/java/com/diver/authserve/AuthServeApplicationTests.java
+++ src/test/java/com/diver/authserve/AuthServeApplicationTests.java
@@ -1,13 +1,16 @@
 package com.diver.authserve;
 
-import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AuthServeApplicationTests {
+class AuthServeApplication {
 
-    @Test
-    void contextLoads() {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServeApplication.class, args);
+        System.out.println("Hello Worldsssss");
     }
+
+
 
 }
